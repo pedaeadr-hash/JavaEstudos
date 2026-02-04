84dcacad1c9613a8b5131856e71e8e42dbe57908
pedaeadr-hash <pedaeadr@gmail.com>
2026-02-04 17:52:49 -0300

    primeiro código Java com main e classes

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("olá, sejam bem vindos");
+
+        int chocolates = 10;
+        Produtos.mostrarProdutos(chocolates);
+    }
+}
---
+++ src/Produtos.java
@@ -0,0 +1,6 @@
+public class Produtos {
+
+    public static void mostrarProdutos(int chocolates) {
+        System.out.println("Quantidade de chocolates: " + chocolates);
+    }
+}
