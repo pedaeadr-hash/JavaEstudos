6267bacb25ba75763e25d989b488266d2066fc95
pedaeadr-hash <pedaeadr@gmail.com>
2026-02-04 18:19:02 -0300

    colocando um pouco de prática

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("olá, sejam bem vindos");
 
-        int chocolates = 10;
-        Produtos.mostrarProdutos(chocolates);
+        String classe = "Pessoa";
+        String nome = "Adrian Ariel Lopes Santos";
+        String sexualidade = "Masculino";
+        int idade = 15;
+        String localidade = "São Paulo";
+        double peso = 64;
+        double altura = 1.70; // exemplo
+
+        System.out.println(
+                classe + " " + nome + " " + idade + " anos " + peso + " kg"
+        );
+
+        double imc = peso / (altura * altura);
+        System.out.println("IMC de aproximadamente: " + imc);
     }
 }
